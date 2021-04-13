afdb16ed70f82f01c76bb6120c432c351631c9ce
AYehia0 <ahmedyehia611@gmail.com>
2021-04-13 19:36:01 +0200

    controlling brightness

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import cv2 
+import subprocess as sp
+import os
 import time
 
 #to find if image is dark or not : simply calculate the average intensity and judge it.
@@ -21,13 +23,14 @@ def capture_img(img_name):
     if ret:
         # Writing the img
         cv2.imwrite(img_name, img)
+        time.sleep(1)
 
     return img
 
 def get_brightness_lvl(img):
     
     # resizing the image so it's easy to deal with 
-    img = cv2.resize(img, (10,10))
+    img = cv2.resize(img, (100,100))
     
     # use the HSL model 
     hsl_model = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
@@ -45,7 +48,21 @@ def is_dark(brt_lvl):
     return True if brt_lvl < BRIGHTNESS_FACTOR else False 
 
 
+def change_brightness(dark_):
+    '''Change the screen brightness'''
+    
+    #get the brightness of the screen 
+    #current_brightness = int(sp.getoutput('xbacklight -get'))
+    current_brightness = 1 
+    
+    choice = "inc" if dark_ == 0 else "dec"
+    os.system(f"xbacklight -{choice} {current_brightness*2}")
 
+    for i in range(current_brightness):
+        os.system(f"xbacklight -{choice} {current_brightness//2}")
+        time.sleep(0.2)
+
+    #exit(0)
 
 def main():
     
@@ -56,8 +73,10 @@ def main():
 
         if dark:
             print(f"Dark : {brightness}")
+            change_brightness(dark)
         else:
             print(f"Bright : {brightness}")
+            change_brightness(dark)
 
         time.sleep(1)
 
